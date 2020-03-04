bf87d59ba9f814a3530bd2754c21f90ba461488e
Ahmed Hussein <wolktheline@yahoo.com>
2020-03-04 04:41:23 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,4 +1,15 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
+  sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS artists (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  genre TEXT,
+  age INTEGER,
+  hometown TEXT
+  )
+SQL
+ 
+ActiveRecord::Base.connection.execute(sql)
   def change
     create_table :artists do |t|
       t.string :name
